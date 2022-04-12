dfd5509eb9f9eff4cfa944c5198b85e74dbc2ec5
unknown <desarrollo305gmail.com>
2022-04-11 21:39:18 -0500

    fadd karate-config.js

---
+++ src/test/java/feature/ConfigDemoRunner.java
@@ -0,0 +1,16 @@
+package feature;
+
+import com.intuit.karate.junit5.Karate;
+import org.junit.jupiter.api.BeforeAll;
+
+public class ConfigDemoRunner {
+
+    @BeforeAll
+    public static void before(){
+        System.setProperty("karate.env", "qa");
+    }
+    @Karate.Test
+    Karate sampleTest(){
+        return Karate.run("configdemo1").relativeTo(getClass());
+    }
+}
